2e45799d3bebbb255734e335e89ee46f594e63d5
lovac42 <lovac42@protonmail.com>
2019-09-07 04:13:58 -0400

    updated support url

--- src/blitzkrieg/__init__.py
+++ src/blitzkrieg/__init__.py
@@ -3,7 +3,7 @@
 # Copyright 2014 Patrice Neff
 # Copyright 2006-2019 Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-# Support: https://github.com/lovac42/HumptyDumpty
+# Support: https://github.com/lovac42/Blitzkrieg
 
 
 from .main import *
