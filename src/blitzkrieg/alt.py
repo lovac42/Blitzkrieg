15500c3da812018e48add6010cf6709998389cc3
lovac42 <lovac42@protonmail.com>
2020-01-01 03:20:20 -0500

    Updated copyright info to include 2020

--- src/blitzkrieg/alt.py
+++ src/blitzkrieg/alt.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 Lovac42
+# Copyright 2019-2020 Lovac42
 # Copyright 2006-2019 Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 # Support: https://github.com/lovac42/Blitzkrieg
